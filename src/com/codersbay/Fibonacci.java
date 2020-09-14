3cb4f91b88fadc5d75610fa22f98e6cf5e1f15d3
marco <kuvimarco@gmail.com>
2020-09-14 15:15:46 +0200

    Add Main Class

--- src/com/codersbay/Fibonacci.java
+++ src/com/codersbay/Fibonacci.java
@@ -1,9 +1,22 @@
 package com.codersbay;
+ import java.util.Scanner;
 
 public class Fibonacci {
 
     public static void main(String[] args) {
-        System.out.println("Hello Fibonacci");
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("What is your number? ");
+        int fibonacciNumber = input.nextInt();
+
+        System.out.println("The " + fibonacciNumber + " Fibonacci Number is: " + fibonacciFormula(fibonacciNumber));
     }
 
+    public static int fibonacciFormula (int number) {
+        if ((number == 0) || (number == 1)) {
+            return number;
+        } else {
+            return fibonacciFormula(number - 1) + fibonacciFormula(number - 2);
+        }
+    }
 }
